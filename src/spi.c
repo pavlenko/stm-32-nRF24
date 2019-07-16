7919a843f1c677977db4ee07320f8cc03c323956
Pavlenko Eugeniy <pavlenko.obs@gmail.com>
2019-07-16 09:32:22 +0300

    add spi msp init/deinit

--- src/spi.c
+++ src/spi.c
@@ -29,18 +29,112 @@ void MX_SPI_Init(SPI_TypeDef *spi, SPI_HandleTypeDef *handle)
 
 void HAL_SPI_MspInit(SPI_HandleTypeDef *spi)
 {
+    GPIO_InitTypeDef GPIO_InitStruct;
+
     if (spi->Instance == SPI1) {
+        /* SPI clock enable */
         __HAL_RCC_SPI1_CLK_ENABLE();
 
-        //TODO GPIO
+        /* GPIO clock enable */
+        __HAL_RCC_GPIOA_CLK_ENABLE();
+
+        /**
+         * SPI GPIO Configuration
+         * PA4 ------> SPI1_NSS
+         * PA5 ------> SPI1_SCK
+         * PA6 ------> SPI1_MISO
+         * PA7 ------> SPI1_MOSI
+         *
+         * Do not initialize PA4/NSS because a software SS will be used
+         */
+        GPIO_InitStruct.Pin   = GPIO_PIN_5|GPIO_PIN_7;
+        GPIO_InitStruct.Mode  = GPIO_MODE_AF_PP;
+        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+
+        HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+        GPIO_InitStruct.Pin   = GPIO_PIN_6;
+        GPIO_InitStruct.Mode  = GPIO_MODE_INPUT;
+        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+
+        HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+        /* SPI interrupt Init */
+        HAL_NVIC_SetPriority(SPI1_IRQn, 1, 1);
+        HAL_NVIC_EnableIRQ(SPI1_IRQn);
+    }
+
+    if (spi->Instance == SPI2) {
+        /* SPI clock enable */
+        __HAL_RCC_SPI1_CLK_ENABLE();
+
+        /* GPIO clock enable */
+        __HAL_RCC_GPIOB_CLK_ENABLE();
+
+        /**
+         * SPI GPIO Configuration
+         * PB12 ------> SPI2_NSS
+         * PB13 ------> SPI2_SCK
+         * PB14 ------> SPI2_MISO
+         * PB15 ------> SPI2_MOSI
+         *
+         * Do not initialize PB12/NSS because a software SS will be used
+         */
+        GPIO_InitStruct.Pin   = GPIO_PIN_13|GPIO_PIN_15;
+        GPIO_InitStruct.Mode  = GPIO_MODE_AF_PP;
+        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+
+        HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+        GPIO_InitStruct.Pin   = GPIO_PIN_14;
+        GPIO_InitStruct.Mode  = GPIO_MODE_INPUT;
+        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+
+        HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+        /* SPI interrupt Init */
+        HAL_NVIC_SetPriority(SPI2_IRQn, 1, 1);
+        HAL_NVIC_EnableIRQ(SPI2_IRQn);
     }
 }
 
 void HAL_SPI_MspDeInit(SPI_HandleTypeDef *spi)
 {
     if (spi->Instance == SPI1) {
+        /* SPI clock disable */
         __HAL_RCC_SPI1_CLK_DISABLE();
 
-        //TODO GPIO
+        /**
+         * SPI GPIO Configuration
+         * PA4 ------> SPI1_NSS
+         * PA5 ------> SPI1_SCK
+         * PA6 ------> SPI1_MISO
+         * PA7 ------> SPI1_MOSI
+         *
+         * Do not de-initialize PB12/NSS because a software SS will be used
+         */
+        HAL_GPIO_DeInit(GPIOA, GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7);
+
+        /* SPI interrupt Deinit */
+        HAL_NVIC_DisableIRQ(SPI1_IRQn);
+    }
+
+    if (spi->Instance == SPI2) {
+        /* SPI clock disable */
+        __HAL_RCC_SPI2_CLK_DISABLE();
+
+        /**
+         * SPI GPIO Configuration
+         * PB12 ------> SPI2_NSS
+         * PB13 ------> SPI2_SCK
+         * PB14 ------> SPI2_MISO
+         * PB15 ------> SPI2_MOSI
+         *
+         * Do not de-initialize PB12/NSS because a software SS will be used
+         */
+        HAL_GPIO_DeInit(GPIOB, GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15);
+
+        /* SPI interrupt Deinit */
+        HAL_NVIC_DisableIRQ(SPI2_IRQn);
     }
 }
