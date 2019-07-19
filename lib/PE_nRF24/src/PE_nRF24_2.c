51d13b0fc67a78e2bbde38ff83632b01de3b42d8
Pavlenko Eugeniy <pavlenko.obs@gmail.com>
2019-07-19 07:52:08 +0300

    complete configure pipe logic v2

--- lib/PE_nRF24/src/PE_nRF24_2.c
+++ lib/PE_nRF24/src/PE_nRF24_2.c
@@ -9,9 +9,31 @@
 /* Private function prototypes -----------------------------------------------*/
 /* Private functions ---------------------------------------------------------*/
 
+static uint8_t PE_nRF24_readByte(uint8_t addr)
+{
+    //TODO
+    (void) addr;
+    return 0;
+}
+
+static void PE_nRF24_sendByte(uint8_t addr, uint8_t byte)
+{
+    //TODO
+    (void) addr;
+    (void) byte;
+}
+
 PE_nRF24_status_t PE_nRF24_initializeRX(PE_nRF24_handle_t *handle, PE_nRF24_configRX_t *config, PE_nRF24_pipe_t pipe)
 {
     uint8_t reg;
+
+    // Enable specific pipe
+    reg = PE_nRF24_readByte(PE_nRF24_REG_EN_RXADDR);
+
+    reg &= ~PE_nRF24_EN_RXADDR_ALL;
+    reg |= (1U << pipe);
+
+    PE_nRF24_sendByte(PE_nRF24_REG_EN_RXADDR, reg);
 
     // Copy ptr to prevent change
     uint8_t *addressValue = config->address;
@@ -39,13 +61,16 @@ PE_nRF24_status_t PE_nRF24_initializeRX(PE_nRF24_handle_t *handle, PE_nRF24_conf
         case PE_nRF24_PIPE_3:
         case PE_nRF24_PIPE_4:
         case PE_nRF24_PIPE_5:
-            // Write address LSBbyte (only first byte from the addr buffer)
+            // Write address LSB byte (only first byte from the addr buffer)
             PE_nRF24_sendByte(PE_nRF24_REG_mX_ADDR_Pn[pipe], *addressValue);
             break;
         default:
             // Incorrect pipe number -> do nothing
             break;
     }
+
+    // Set RX payload length (RX_PW_Px register)
+    PE_nRF24_sendByte(PE_nRF24_REG_RX_PW_Pn[pipe], config->payloadSize & 0x3FU);
 
     // Configure Auto Acknowledgement
     reg = PE_nRF24_readByte(PE_nRF24_REG_EN_AA);
