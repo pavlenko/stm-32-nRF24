e8223ae34ea6b3cf361539ad8f76429e42f9f39b
Pavlenko Eugeniy <pavlenko.obs@gmail.com>
2019-07-23 13:38:10 +0300

    update macro usage & add __ prefix for prevent conflict with function names

--- lib/PE_nRF24/src/PE_nRF24.c
+++ lib/PE_nRF24/src/PE_nRF24.c
@@ -9,24 +9,6 @@
 /* Private function prototypes -----------------------------------------------*/
 /* Private functions ---------------------------------------------------------*/
 
-static PE_nRF24_STATUS_t PE_nRF24_readByte(PE_nRF24_t *handle, uint8_t addr, uint8_t *byte)
-{
-    //TODO
-    (void) handle;
-    (void) addr;
-    (void) byte;
-    return PE_nRF24_STATUS_OK;
-}
-
-static PE_nRF24_STATUS_t PE_nRF24_sendByte(PE_nRF24_t *handle, uint8_t addr, uint8_t *byte)
-{
-    //TODO
-    (void) handle;
-    (void) addr;
-    (void) byte;
-    return PE_nRF24_STATUS_OK;
-}
-
 //TODO create functions like i2c_mem_read, i2c_mem_write
 
 static PE_nRF24_STATUS_t PE_nRF24_getRegister(PE_nRF24_t *handle, uint8_t addr, uint8_t *byte)
@@ -67,22 +49,50 @@ static PE_nRF24_STATUS_t PE_nRF24_setRegister(PE_nRF24_t *handle, uint8_t addr, 
     return PE_nRF24_STATUS_OK;
 }
 
-#define PE_nRF24_setDirection(handle, value) \
+#define __PE_nRF24_flushTX(handle) \
     do { \
-        uint8_t reg; \
-        if (PE_nRF24_getRegister(handle, PE_nRF24_REG_CONFIG, &reg) != PE_nRF24_STATUS_OK) return PE_nRF24_STATUS_ERROR; \
-        reg &= ~PE_nRF24_CONFIG_PRIM_RX; \
-        reg |= ((value & 0x1U) << PE_nRF24_CONFIG_PRIM_RX_Pos); \
-        if (PE_nRF24_setRegister(handle, PE_nRF24_REG_CONFIG, &reg) != PE_nRF24_STATUS_OK) return PE_nRF24_STATUS_ERROR; \
+        uint8_t addr = PE_nRF24_CMD_FLUSH_TX; \
+        if (handle->send(&addr, 1) != PE_nRF24_STATUS_OK) { \
+            handle->status = PE_nRF24_STATUS_ERROR; \
+        } \
     } while (0U);
 
-#define PE_nRF24_setPowerMode(handle, value) \
+#define __PE_nRF24_flushRX(handle) \
+    do { \
+        uint8_t addr = PE_nRF24_CMD_FLUSH_RX; \
+        if (handle->send(&addr, 1) != PE_nRF24_STATUS_OK) { \
+            handle->status = PE_nRF24_STATUS_ERROR; \
+        } \
+    } while (0U);
+
+#define __PE_nRF24_setDirection(handle, value) \
     do { \
         uint8_t reg; \
-        if (PE_nRF24_getRegister(handle, PE_nRF24_REG_CONFIG, &reg) != PE_nRF24_STATUS_OK) return PE_nRF24_STATUS_ERROR; \
+        if (PE_nRF24_getRegister(handle, PE_nRF24_REG_CONFIG, &reg) != PE_nRF24_STATUS_OK) { \
+            handle->status = PE_nRF24_STATUS_ERROR; \
+            break; \
+        } \
+        reg &= ~PE_nRF24_CONFIG_PRIM_RX; \
+        reg |= ((value & 0x1U) << PE_nRF24_CONFIG_PRIM_RX_Pos); \
+        if (PE_nRF24_setRegister(handle, PE_nRF24_REG_CONFIG, &reg) != PE_nRF24_STATUS_OK) { \
+            handle->status = PE_nRF24_STATUS_ERROR; \
+            break; \
+        } \
+    } while (0U);
+
+#define __PE_nRF24_setPowerMode(handle, value) \
+    do { \
+        uint8_t reg; \
+        if (PE_nRF24_getRegister(handle, PE_nRF24_REG_CONFIG, &reg) != PE_nRF24_STATUS_OK) { \
+            handle->status = PE_nRF24_STATUS_ERROR; \
+            break; \
+        } \
         reg &= ~PE_nRF24_CONFIG_PWR_UP; \
         reg |= ((value & 0x1U) << PE_nRF24_CONFIG_PWR_UP_Pos); \
-        if (PE_nRF24_setRegister(handle, PE_nRF24_REG_CONFIG, &reg) != PE_nRF24_STATUS_OK) return PE_nRF24_STATUS_ERROR; \
+        if (PE_nRF24_setRegister(handle, PE_nRF24_REG_CONFIG, &reg) != PE_nRF24_STATUS_OK) { \
+            handle->status = PE_nRF24_STATUS_ERROR; \
+            break; \
+        } \
     } while (0U);
 
 
@@ -90,7 +100,7 @@ PE_nRF24_STATUS_t PE_nRF24_handleIRQ(PE_nRF24_t *handle)
 {
     uint8_t status;
 
-    if (PE_nRF24_readByte(handle, PE_nRF24_REG_STATUS, &status) != PE_nRF24_STATUS_OK) {
+    if (PE_nRF24_getRegister(handle, PE_nRF24_REG_STATUS, &status) != PE_nRF24_STATUS_OK) {
         return PE_nRF24_STATUS_ERROR;
     }
 
@@ -112,8 +122,8 @@ PE_nRF24_STATUS_t PE_nRF24_handleIRQ(PE_nRF24_t *handle)
 
             status |= PE_nRF24_STATUS_RX_DR;
 
-            PE_nRF24_sendByte(handle, PE_nRF24_REG_STATUS, &status);
-            PE_nRF24_readByte(handle, PE_nRF24_REG_FIFO_STATUS, &statusFIFO);
+            PE_nRF24_setRegister(handle, PE_nRF24_REG_STATUS, &status);
+            PE_nRF24_getRegister(handle, PE_nRF24_REG_FIFO_STATUS, &statusFIFO);
         } while ((statusFIFO & PE_nRF24_FIFO_STATUS_RX_EMPTY) == 0x00);
 
         handle->setCE(1);
@@ -125,8 +135,8 @@ PE_nRF24_STATUS_t PE_nRF24_handleIRQ(PE_nRF24_t *handle)
 
         status |= PE_nRF24_STATUS_TX_DS;
 
-        PE_nRF24_setDirection(handle, PE_nRF24_DIRECTION_RX);
-        PE_nRF24_sendByte(handle, PE_nRF24_REG_STATUS, &status);
+        __PE_nRF24_setDirection(handle, PE_nRF24_DIRECTION_RX);
+        PE_nRF24_setRegister(handle, PE_nRF24_REG_STATUS, &status);
 
         handle->setCE(1);
 
@@ -137,16 +147,16 @@ PE_nRF24_STATUS_t PE_nRF24_handleIRQ(PE_nRF24_t *handle)
     if ((status & PE_nRF24_STATUS_MAX_RT) != 0) {
         status |= PE_nRF24_STATUS_MAX_RT;
 
-        //TODO PE_nRF24_flushTX(handle);
+        __PE_nRF24_flushTX(handle);
 
         // Toggle RF power up bit
-        PE_nRF24_setPowerMode(handle, PE_nRF24_POWER_OFF);
-        PE_nRF24_setPowerMode(handle, PE_nRF24_POWER_ON);
+        __PE_nRF24_setPowerMode(handle, PE_nRF24_POWER_OFF);
+        __PE_nRF24_setPowerMode(handle, PE_nRF24_POWER_ON);
 
         handle->setCE(0);
 
-        PE_nRF24_setDirection(handle, PE_nRF24_DIRECTION_RX);
-        PE_nRF24_sendByte(handle, PE_nRF24_REG_STATUS, &status);
+        __PE_nRF24_setDirection(handle, PE_nRF24_DIRECTION_RX);
+        PE_nRF24_setRegister(handle, PE_nRF24_REG_STATUS, &status);
 
         handle->setCE(1);
 
